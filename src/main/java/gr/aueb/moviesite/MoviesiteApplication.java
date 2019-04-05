db4a7f713a0e04e3de2fc119b9c6ee23dd7cb884
eskiada <eskiada@cognity.gr>
2019-04-05 21:04:10 +0300

    second commit

---
+++ src/main/java/gr/aueb/moviesite/MoviesiteApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.moviesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesiteApplication.class, args);
+	}
+
+}
